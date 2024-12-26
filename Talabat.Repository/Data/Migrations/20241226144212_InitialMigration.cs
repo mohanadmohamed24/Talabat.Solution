9e8f4577759acde88d5f075c02325b6d8c08788e
mohanadmohamed24 <mohanad_mohamed78@yahoo.com>
2024-12-26 17:12:12 +0200

    Add project files.

---
+++ Talabat.Repository/Data/Migrations/20241226144212_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Talabat.Repository.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DeliveryMethodId",
+                table: "Order",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DeliveryMethodId",
+                table: "Order",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
